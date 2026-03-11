e5fb66b77b01299fcda7aef1332623944f448c65
kris <kristonwang02@gmail.com>
2026-03-10 23:58:11 -0400

    Added embedding.py with implemented Embedding class

--- assignment1-basics/implementation/linear.py
+++ assignment1-basics/implementation/linear.py
@@ -10,7 +10,8 @@ class Linear(nn.Module):
         self.weight = nn.Parameter(
             torch.empty((out_features, in_features), device=device, dtype=dtype)
         )
-        nn.init.trunc_normal_(self.weight)
+        std = 2 / (in_features + out_features)
+        nn.init.trunc_normal_(self.weight, mean = 0.0, std = std, a = -3 * std ** 0.5, b = 3 * std ** 0.5)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x @ self.weight.T
