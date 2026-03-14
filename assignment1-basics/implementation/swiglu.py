a131d8a0f12dce864be5930ace90691b713ecd20
kris <kristonwang02@gmail.com>
2026-03-14 00:32:10 -0400

    add changes to adapters and test for showing underlying storage of the same tensor whether reshaped are the same

--- assignment1-basics/implementation/swiglu.py
+++ assignment1-basics/implementation/swiglu.py
@@ -1,14 +1,25 @@
 import torch
 
 class SwiGLU(torch.nn.Module):
-    def __init__(self, d_model: int, device=None, dtype=None):
+    def __init__(self, d_model: int, d_ff: int, device=None, dtype=None):
         super().__init__()
 
-        self.w1 = torch.nn.Parameter(torch.empty((d_model, 3/8 * d_model), device=device, dtype=dtype))
-        self.w3 = torch.nn.Parameter(torch.empty((d_model, 3/8 * d_model), device=device, dtype=dtype))
-        self.w2 = torch.nn.Parameter(torch.empty((3/8 * d_model, d_model), device=device, dtype=dtype))
+        self.w1 = torch.nn.Parameter(torch.empty((d_ff, d_model), device=device, dtype=dtype))
+        self.w3 = torch.nn.Parameter(torch.empty((d_ff, d_model), device=device, dtype=dtype))
+        self.w2 = torch.nn.Parameter(torch.empty((d_model, d_ff), device=device, dtype=dtype))
 
-
+    # input: (batch_size, seq_len, d_model)
+    # W2 @ (SiLU(W1 @ x) * (W3 @ x))
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.w2 @ (torch.sigmoid(self.w1 @ x) * (self.w3 @ x))
-    
+        w1x = x @ self.w1.T
+        return (torch.sigmoid(w1x) * (w1x) * (x @ self.w3.T)) @ self.w2.T
+
+# def test():
+#     a = torch.arange(1, 10).reshape(3, 3).float()
+#     b = a.T
+#     c = a.transpose(0, 1)
+#     print(id(a), id(b), id(c)) # ids are different, but all three objects share the same underlying data
+#     print(a.is_contiguous(), b.is_contiguous(), c.is_contiguous())  # a is contiguous, but b and c are not
+#     print(a.storage().data_ptr(), b.storage().data_ptr(), c.storage().data_ptr())  # all point to the same underlying data
+
+# test()
--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -91,7 +91,14 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+
+    from implementation.swiglu import SwiGLU
+    swiglu = SwiGLU(d_model, d_ff)
+    swiglu.w1.data = w1_weight
+    swiglu.w2.data = w2_weight
+    swiglu.w3.data = w3_weight
+
+    return swiglu(in_features)
 
 
 def run_scaled_dot_product_attention(
