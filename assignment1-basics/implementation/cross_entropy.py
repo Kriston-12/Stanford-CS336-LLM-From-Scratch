f061632f3bd0e2073209833cfc2532f8c75c1e64
kris <kristonwang02@gmail.com>
2026-03-28 23:55:01 -0400

    use log_softmax to avoid safe softmax underflow

--- assignment1-basics/implementation/cross_entropy.py
+++ assignment1-basics/implementation/cross_entropy.py
@@ -1,17 +1,19 @@
 import torch
 import torch.nn as nn
 from torch import Tensor
-from implementation.softmax import Softmax
 
 class CrossEntropyLoss(nn.Module):
-    def __init__(self):
-        super.__init__()
-        self.softmax = Softmax(dim=-1)
-    
-    def forward(self, x: Tensor):
-        logits = self.softmax(x)
-        ls = x.shape[:-1]
-        total_elements_to_average = 1
-        for d in ls:
-            total_elements_to_average *= d
-        return torch.sum(torch.log(logits)) / total_elements_to_average
+    def __init__(self) -> None:
+        super().__init__()
+
+    '''Args:
+        inputs (Float[Tensor, "batch_size vocab_size"]): inputs[i][j] is the
+            unnormalized logit of jth class for the ith example.
+        targets (Int[Tensor, "batch_size"]): Tensor of shape (batch_size,) with the index of the correct class.
+            Each value must be between 0 and `num_classes - 1`.
+
+    Returns:
+        Float[Tensor, ""]: The average cross-entropy loss across examples.'''
+    def forward(self, x: Tensor, targets: Tensor):
+        log_probs = x.log_softmax(dim=-1)
+        return (-log_probs[torch.arange(x.shape[0]), targets]).mean()
