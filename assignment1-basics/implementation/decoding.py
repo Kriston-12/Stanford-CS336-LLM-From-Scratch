4777f254d65f653c1338afa2ddb25bd0a81b45ff
kris <kristonwang02@gmail.com>
2026-04-08 23:58:56 -0400

    done with decoding

---
+++ assignment1-basics/implementation/decoding.py
@@ -0,0 +1,60 @@
+
+from __future__ import annotations
+import torch
+
+from implementation.transformer_LM import Transformer
+
+
+def sample_next_token(
+    logits: torch.Tensor,  # (batch_size, vocab_size)
+    temperature: float = 1.0,
+    top_p_threshold: float = 1.0,
+) -> torch.Tensor:
+
+    probs = torch.softmax(logits / temperature, dim=-1)
+
+    if top_p_threshold < 1.0:
+        sorted_probs, sorted_indices = torch.sort(probs, dim=-1, descending=True)
+        cumulative_probs = torch.cumsum(sorted_probs, dim=-1)
+
+        remove_mask = cumulative_probs > top_p_threshold
+        remove_mask[..., 1:] = remove_mask[..., :-1].clone()
+        remove_mask[..., 0] = False
+
+        sorted_probs = sorted_probs.masked_fill(remove_mask, 0.0)
+        sorted_probs = sorted_probs / sorted_probs.sum(dim=-1, keepdim=True)
+
+        sampled_sorted_positions = torch.multinomial(sorted_probs, num_samples=1)
+        return torch.gather(sorted_indices, dim=-1, index=sampled_sorted_positions)
+
+    return torch.multinomial(probs, num_samples=1)
+
+
+def decode(
+    model: Transformer,
+    prompt_token_ids: torch.Tensor,  # (batch_size, sequence_length)
+    max_number_of_tokens: int,
+    temperature: float = 1.0,
+    top_p_threshold: float = 1.0,
+    end_token_ids: list[int] | None = None,
+) -> torch.Tensor:
+
+    generated = prompt_token_ids
+    context_length = context_length = model.transformer_blocks[0].attn.max_seq_len
+    eos_ids = None if end_token_ids is None else torch.tensor(end_token_ids, device=generated.device)
+
+    for _ in range(max_number_of_tokens):
+        model_input = generated if context_length is None else generated[:, -context_length:]
+        logits = model(model_input)[:, -1, :]  # (batch_size, vocab_size)
+        next_token_ids = sample_next_token(
+            logits=logits,
+            temperature=temperature,
+            top_p_threshold=top_p_threshold,
+        )
+
+        generated = torch.cat([generated, next_token_ids], dim=1)
+
+        if eos_ids is not None and torch.isin(next_token_ids, eos_ids).all():
+            break
+
+    return generated
